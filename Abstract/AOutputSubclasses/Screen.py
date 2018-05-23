808cae83865a47bfbd3350e249386b4d2eaa0789
Francisco Dominguez <francisco.dominguez@urjc.es>
2018-05-23 20:57:08 +0200

    Hola Antonny. Ya habla

--- Abstract/AOutputSubclasses/Screen.py
+++ Abstract/AOutputSubclasses/Screen.py
@@ -1,8 +1,12 @@
 from Abstract.AOutput import IOutput
+import os
 
 class CScreen(IOutput):
 
     def exec(self,sentence):
+        sentence=sentence.replace('"','')
+        command='~/di.sh  "%s."'%sentence
+        os.system(command)
         print(sentence)
 
         
